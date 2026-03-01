4969226a21d0fff2b6c61cb0169cc60a8912c329
carlos-eduardo-ip <carlos.eduardog2p@gmail.com>
2026-03-01 15:47:27 -0300

    refactor: Garantir que os diretórios necessários sejam criados automaticamente na config.

--- automacao_selenium_kaggle/src/common/config.py
+++ automacao_selenium_kaggle/src/common/config.py
@@ -6,11 +6,22 @@ from pathlib import Path
 load_dotenv()
 
 class Config:
+    BASE_DIR = Path(__file__).resolve().parents[2]
+
     KAGGLE_USER = os.getenv("KAGGLE_USER")
     KAGGLE_PASSWORD = os.getenv("KAGGLE_PASSWORD")
     CHROME_DRIVER_PATH = os.getenv("CHROME_DRIVER_PATH")
     LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
-    USER_DATA_DIR = Path(__file__).resolve().parents[2] / "data" / "user_data"
-    DOWNLOAD_DIR = Path(__file__).resolve().parents[2] / "data" / "downloads"
+
+    USER_DATA_DIR = BASE_DIR / "data" / "user_data"
+    DOWNLOAD_DIR = BASE_DIR / "data" / "downloads"
+    LOGS_DIR = BASE_DIR / "logs"
+
+    @classmethod
+    def ensure_directories(cls):
+        cls.USER_DATA_DIR.mkdir(parents=True, exist_ok=True)
+        cls.DOWNLOAD_DIR.mkdir(parents=True, exist_ok=True)
+        cls.LOGS_DIR.mkdir(parents=True, exist_ok=True)
 
 config = Config()
+config.ensure_directories()
